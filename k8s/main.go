c408bbdd28cbbb9bb44dbb048e53dde8eb0a7666
Thomas Schenker <tommzn@gmx.de>
2022-01-06 01:38:07 +0100

    add file log

--- k8s/main.go
+++ k8s/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
+
+	IOReadDir("/go/")
+	IOReadDir("/go/templates/")
 
 	ctx := context.Background()
 	minion, bootstrapError := bootstrap(nil, ctx)
@@ -15,5 +22,15 @@ func main() {
 func exitOnError(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func IOReadDir(root string) {
+	fileInfo, err := ioutil.ReadDir(root)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, file := range fileInfo {
+		fmt.Println(file.Name())
 	}
 }
