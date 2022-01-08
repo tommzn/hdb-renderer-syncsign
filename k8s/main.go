5d0fb03b480863cc5d30c99a8db3d893cbf74dca
Thomas Schenker <tommzn@gmx.de>
2022-01-08 23:28:57 +0100

    remove debug logs

--- k8s/main.go
+++ k8s/main.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"io/ioutil"
 )
 
 func main() {
-
-	IOReadDir("/go/")
-	IOReadDir("/go/templates/")
 
 	ctx := context.Background()
 	minion, bootstrapError := bootstrap(nil, ctx)
@@ -22,15 +17,5 @@ func main() {
 func exitOnError(err error) {
 	if err != nil {
 		panic(err)
-	}
-}
-
-func IOReadDir(root string) {
-	fileInfo, err := ioutil.ReadDir(root)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, file := range fileInfo {
-		fmt.Println(file.Mode(), " ", root, "/", file.Name())
 	}
 }
